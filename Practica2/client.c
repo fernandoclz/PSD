ba377f4484c9682e3b6f82a8b0b1e93e5b452579
fernandoclz <fernandoliuzhang@gmail.com>
2025-10-16 18:45:02 +0200

    16/10

--- Practica2/client.c
+++ Practica2/client.c
@@ -1,94 +1,97 @@
 #include "client.h"
 
-unsigned int readBet (){
+unsigned int readBet()
+{
 
-	int isValid, bet=0;
+	int isValid, bet = 0;
 	xsd__string enteredMove;
 
-		// While player does not enter a correct bet...
-		do{
+	// While player does not enter a correct bet...
+	do
+	{
 
-			// Init...
-			enteredMove = (xsd__string) malloc (STRING_LENGTH);
-			bzero (enteredMove, STRING_LENGTH);
-			isValid = TRUE;
+		// Init...
+		enteredMove = (xsd__string)malloc(STRING_LENGTH);
+		bzero(enteredMove, STRING_LENGTH);
+		isValid = TRUE;
 
-			printf ("Enter a value:");
-			fgets(enteredMove, STRING_LENGTH-1, stdin);
-			enteredMove[strlen(enteredMove)-1] = 0;
+		printf("Enter a value:");
+		fgets(enteredMove, STRING_LENGTH - 1, stdin);
+		enteredMove[strlen(enteredMove) - 1] = 0;
 
-			// Check if each character is a digit
-			for (int i=0; i<strlen(enteredMove) && isValid; i++)
-				if (!isdigit(enteredMove[i]))
-					isValid = FALSE;
+		// Check if each character is a digit
+		for (int i = 0; i < strlen(enteredMove) && isValid; i++)
+			if (!isdigit(enteredMove[i]))
+				isValid = FALSE;
 
-			// Entered move is not a number
-			if (!isValid)
-				printf ("Entered value is not correct. It must be a number greater than 0\n");
-			else
-				bet = atoi (enteredMove);
+		// Entered move is not a number
+		if (!isValid)
+			printf("Entered value is not correct. It must be a number greater than 0\n");
+		else
+			bet = atoi(enteredMove);
 
-		}while (!isValid);
+	} while (!isValid);
 
-		printf ("\n");
-		free (enteredMove);
+	printf("\n");
+	free(enteredMove);
 
-	return ((unsigned int) bet);
+	return ((unsigned int)bet);
 }
 
-unsigned int readOption (){
+unsigned int readOption()
+{
 
 	unsigned int bet;
 
-		do{
-			printf ("What is your move? Press %d to hit a card and %d to stand\n", PLAYER_HIT_CARD, PLAYER_STAND);
-			bet = readBet();
-			if ((bet != PLAYER_HIT_CARD) && (bet != PLAYER_STAND))
-				printf ("Wrong option!\n");			
-		} while ((bet != PLAYER_HIT_CARD) && (bet != PLAYER_STAND));
+	do
+	{
+		printf("What is your move? Press %d to hit a card and %d to stand\n", PLAYER_HIT_CARD, PLAYER_STAND);
+		bet = readBet();
+		if ((bet != PLAYER_HIT_CARD) && (bet != PLAYER_STAND))
+			printf("Wrong option!\n");
+	} while ((bet != PLAYER_HIT_CARD) && (bet != PLAYER_STAND));
 
 	return bet;
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
-	struct soap soap;					/** Soap struct */
-	char *serverURL;					/** Server URL */
-	blackJackns__tMessage playerName;	/** Player name */
-	blackJackns__tBlock gameStatus;		/** Game status */
-	unsigned int playerMove;			/** Player's move */
-	int resCode, gameId;				/** Result and gameId */
-	
-		// Init gSOAP environment
-		soap_init(&soap);
+	struct soap soap;				  /** Soap struct */
+	char *serverURL;				  /** Server URL */
+	blackJackns__tMessage playerName; /** Player name */
+	blackJackns__tBlock gameStatus;	  /** Game status */
+	unsigned int playerMove;		  /** Player's move */
+	int resCode, gameId;			  /** Result and gameId */
 
-		// Obtain server address
-		serverURL = argv[1];
+	// Init gSOAP environment
+	soap_init(&soap);
 
-		// Allocate memory
-		allocClearMessage (&soap, &(playerName));
-		allocClearBlock (&soap, &gameStatus);
-				
-		// Check arguments
-		if (argc !=2) {
-			printf("Usage: %s http://server:port\n",argv[0]);
-			exit(0);
-		}
+	// Obtain server address
+	serverURL = argv[1];
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	// Allocate memory
+	allocClearMessage(&soap, &(playerName));
+	allocClearBlock(&soap, &gameStatus);
 
-  	return 0;
+	// Check arguments
+	if (argc != 2)
+	{
+		printf("Usage: %s http://server:port\n", argv[0]);
+		exit(0);
+	}
+
+	/*
+	Mientras (jugador no registrado)
+registrar(nombreJugador, &idPartida)
+Mientras (no acabe el juego)
+getStatus (nombreJugador, idPartida, …);
+Imprimir estado del juego
+Mientras (jugador tiene el turno)
+jugada = Leer opción del jugador
+playerMove (nombreJugador,idPartida, jugada, …)
+Imprimir estado del juego
+	*/
+
+	return 0;
 }
--- Practica2/server.c
+++ Practica2/server.c
@@ -3,141 +3,151 @@
 /** Shared array that contains all the games. */
 tGame games[MAX_GAMES];
 
-void initGame (tGame *game){
+void initGame(tGame *game)
+{
 
 	// Init players' name
-	memset (game->player1Name, 0, STRING_LENGTH);	
-	memset (game->player2Name, 0, STRING_LENGTH);
+	memset(game->player1Name, 0, STRING_LENGTH);
+	memset(game->player2Name, 0, STRING_LENGTH);
 
-	// Alloc memory for the decks		
-	clearDeck (&(game->player1Deck));	
-	clearDeck (&(game->player2Deck));	
-	initDeck (&(game->gameDeck));
-	
+	// Alloc memory for the decks
+	clearDeck(&(game->player1Deck));
+	clearDeck(&(game->player2Deck));
+	initDeck(&(game->gameDeck));
+
 	// Bet and stack
 	game->player1Bet = 0;
 	game->player2Bet = 0;
 	game->player1Stack = INITIAL_STACK;
 	game->player2Stack = INITIAL_STACK;
-	
+
 	// Game status variables
 	game->endOfGame = FALSE;
 	game->status = gameEmpty;
 }
 
-void initServerStructures (struct soap *soap){
+void initServerStructures(struct soap *soap)
+{
 
 	if (DEBUG_SERVER)
-		printf ("Initializing structures...\n");
+		printf("Initializing structures...\n");
 
 	// Init seed
-	srand (time(NULL));
+	srand(time(NULL));
 
 	// Init each game (alloc memory and init)
-	for (int i=0; i<MAX_GAMES; i++){
-		games[i].player1Name = (xsd__string) soap_malloc (soap, STRING_LENGTH);
-		games[i].player2Name = (xsd__string) soap_malloc (soap, STRING_LENGTH);
-		allocDeck(soap, &(games[i].player1Deck));	
-		allocDeck(soap, &(games[i].player2Deck));	
+	for (int i = 0; i < MAX_GAMES; i++)
+	{
+		games[i].player1Name = (xsd__string)soap_malloc(soap, STRING_LENGTH);
+		games[i].player2Name = (xsd__string)soap_malloc(soap, STRING_LENGTH);
+		allocDeck(soap, &(games[i].player1Deck));
+		allocDeck(soap, &(games[i].player2Deck));
 		allocDeck(soap, &(games[i].gameDeck));
-		initGame (&(games[i]));
-	}	
+		initGame(&(games[i]));
+	}
 }
 
-void initDeck (blackJackns__tDeck *deck){
+void initDeck(blackJackns__tDeck *deck)
+{
 
 	deck->__size = DECK_SIZE;
 
-	for (int i=0; i<DECK_SIZE; i++)
+	for (int i = 0; i < DECK_SIZE; i++)
 		deck->cards[i] = i;
 }
 
-void clearDeck (blackJackns__tDeck *deck){
+void clearDeck(blackJackns__tDeck *deck)
+{
 
 	// Set number of cards
 	deck->__size = 0;
 
-	for (int i=0; i<DECK_SIZE; i++)
+	for (int i = 0; i < DECK_SIZE; i++)
 		deck->cards[i] = UNSET_CARD;
 }
 
-tPlayer calculateNextPlayer (tPlayer currentPlayer){
+tPlayer calculateNextPlayer(tPlayer currentPlayer)
+{
 	return ((currentPlayer == player1) ? player2 : player1);
 }
 
-unsigned int getRandomCard (blackJackns__tDeck* deck){
+unsigned int getRandomCard(blackJackns__tDeck *deck)
+{
 
 	unsigned int card, cardIndex, i;
 
-		// Get a random card
-		cardIndex = rand() % deck->__size;
-		card = deck->cards[cardIndex];
+	// Get a random card
+	cardIndex = rand() % deck->__size;
+	card = deck->cards[cardIndex];
 
-		// Remove the gap
-		for (i=cardIndex; i<deck->__size-1; i++)
-			deck->cards[i] = deck->cards[i+1];
+	// Remove the gap
+	for (i = cardIndex; i < deck->__size - 1; i++)
+		deck->cards[i] = deck->cards[i + 1];
 
-		// Update the number of cards in the deck
-		deck->__size--;
-		deck->cards[deck->__size] = UNSET_CARD;
+	// Update the number of cards in the deck
+	deck->__size--;
+	deck->cards[deck->__size] = UNSET_CARD;
 
 	return card;
 }
 
-unsigned int calculatePoints (blackJackns__tDeck *deck){
+unsigned int calculatePoints(blackJackns__tDeck *deck)
+{
 
 	unsigned int points = 0;
-		
-		for (int i=0; i<deck->__size; i++){
 
-			if (deck->cards[i] % SUIT_SIZE < 9)
-				points += (deck->cards[i] % SUIT_SIZE) + 1;
-			else
-				points += FIGURE_VALUE;
-		}
+	for (int i = 0; i < deck->__size; i++)
+	{
+
+		if (deck->cards[i] % SUIT_SIZE < 9)
+			points += (deck->cards[i] % SUIT_SIZE) + 1;
+		else
+			points += FIGURE_VALUE;
+	}
 
 	return points;
 }
 
-void copyGameStatusStructure (blackJackns__tBlock* status, char* message, blackJackns__tDeck *newDeck, int newCode){
+void copyGameStatusStructure(blackJackns__tBlock *status, char *message, blackJackns__tDeck *newDeck, int newCode)
+{
 
 	// Copy the message
 	memset((status->msgStruct).msg, 0, STRING_LENGTH);
-	strcpy ((status->msgStruct).msg, message);
-	(status->msgStruct).__size = strlen ((status->msgStruct).msg);
+	strcpy((status->msgStruct).msg, message);
+	(status->msgStruct).__size = strlen((status->msgStruct).msg);
 
 	// Copy the deck, only if it is not NULL
 	if (newDeck->__size > 0)
-		memcpy ((status->deck).cards, newDeck->cards, DECK_SIZE*sizeof (unsigned int));	
+		memcpy((status->deck).cards, newDeck->cards, DECK_SIZE * sizeof(unsigned int));
 	else
 		(status->deck).cards = NULL;
 
 	(status->deck).__size = newDeck->__size;
 
 	// Set the new code
-	status->code = newCode;	
+	status->code = newCode;
 }
 
+int blackJackns__register(struct soap *soap, blackJackns__tMessage playerName, int *result)
+{
 
-
-int blackJackns__register (struct soap *soap, blackJackns__tMessage playerName, int* result){
-		
 	int gameIndex;
 
-		// Set \0 at the end of the string
-		playerName.msg[playerName.__size] = 0;
+	// Set \0 at the end of the string
+	playerName.msg[playerName.__size] = 0;
 
-		if (DEBUG_SERVER)
-			printf ("[Register] Registering new player -> [%s]\n", playerName.msg);
+	if (DEBUG_SERVER)
+		printf("[Register] Registering new player -> [%s]\n", playerName.msg);
 
-	
-	
-	
-
-  	return SOAP_OK;
+	return SOAP_OK;
 }
 
-int main(int argc, char **argv){ 
+void *processRequest(void *soap)
+{
+}
+
+int main(int argc, char **argv)
+{
 
 	struct soap soap;
 	struct soap *tsoap;
@@ -145,12 +155,70 @@ int main(int argc, char **argv){
 	int port;
 	SOAP_SOCKET m, s;
 
-		// Check arguments
-		if (argc !=2) {
-			printf("Usage: %s port\n",argv[0]);
-			exit(0);
+	// Check arguments
+	if (argc != 2)
+	{
+		printf("Usage: %s port\n", argv[0]);
+		exit(0);
+	}
+
+	soap_init(&soap);
+
+	// Configure timeouts
+	soap.send_timeout = 60;		// 60 seconds
+	soap.recv_timeout = 60;		// 60 seconds
+	soap.accept_timeout = 3600; // server stops after 1 hour of inactivity
+	soap.max_keep_alive = 100;	// max keep-alive sequence
+
+	// Get listening port
+	port = atoi(argv[1]);
+
+	// Bind
+	m = soap_bind(&soap, NULL, port, 100);
+
+	if (!soap_valid_socket(m))
+	{
+		exit(1);
+	}
+
+	printf("Server is ON!\n");
+	initServerStructures(&soap);
+
+	while (TRUE)
+	{
+
+		// Accept a new connection
+		s = soap_accept(&soap);
+
+		// Socket is not valid :(
+		if (!soap_valid_socket(s))
+		{
+
+			if (soap.errnum)
+			{
+				soap_print_fault(&soap, stderr);
+				exit(1);
+			}
+
+			fprintf(stderr, "Time out!\n");
+			break;
 		}
 
-	
+		// Copy the SOAP environment
+		tsoap = soap_copy(&soap);
+
+		if (!tsoap)
+		{
+			printf("SOAP copy error!\n");
+			break;
+		}
+
+		// Create a new thread to process the request
+		pthread_create(&tid, NULL, (void *(*)(void *))processRequest, (void *)tsoap);
+	}
+
+	// Detach SOAP environment
+	soap_done(&soap);
+
 	return 0;
 }
